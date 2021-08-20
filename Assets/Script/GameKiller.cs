97259e1fa5b259cf0b10903a7e5ff636ef4f7642
unknown <lf3.net@gmail.com>
2021-08-20 21:29:25 +0800

    health point

--- Assets/Script/GameKiller.cs
+++ Assets/Script/GameKiller.cs
@@ -15,7 +15,14 @@ public class GameKiller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hp == 2)
+        {
+            Destroy(GameObject.FindWithTag("healthpoint2"));
+        }
+        if (hp == 1)
+        {
+            Destroy(GameObject.FindWithTag("healthpoint1"));
+        }
     }
 
     // Kill the tetros and deduce the blood
