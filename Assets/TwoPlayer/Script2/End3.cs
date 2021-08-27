a8b5577a8c56690f131152423456c4ae171de383
unknown <lf3.net@gmail.com>
2021-08-27 21:01:27 +0800

    double-player win

---
+++ Assets/TwoPlayer/Script2/End3.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End3 : MonoBehaviour
+{
+    // public
+    public int windowWidth = 400;
+    public int windowHight = 150;
+
+    // private
+    Rect windowRect;
+    int windowSwitch = 0;
+    float alpha = 0;
+
+    void GUIAlphaColor_0_To_1()
+    {
+        if (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            GUI.color = new Color(1, 1, 1, 1); //想要淡出效果的話，先把暫停(Time.timeScale = 0f;)關掉，然後這裡修改成 GUI.color = new Color(1, 1, 1, alpha);
+        }
+    }
+
+    // Init
+    void Awake()
+    {
+        windowRect = new Rect(
+            (Screen.width - windowWidth) / 2,
+            (Screen.height - windowHight) / 2,
+            windowWidth,
+            windowHight);
+    }
+
+    void Update()
+    {
+        //以下是原本設定按下esc鍵之後會出現的後果
+        //if (Input.GetKeyDown("escape"))
+        //{
+        //    windowSwitch = 1;
+        //    alpha = 0; // Init Window Alpha Color
+        //    Time.timeScale = 0f;
+        //}
+    }
+
+    //If win is triggered from TetroControl/void OnTriggerEnter2D(Collider2D other)
+    public void EndWin()
+    {
+        windowSwitch = 1;
+        alpha = 0; // Init Window Alpha Color
+        Time.timeScale = 0f;
+    }
+
+    void OnGUI()
+    {
+        if (windowSwitch == 1)
+        {
+            GUIAlphaColor_0_To_1();
+            windowRect = GUI.Window(0, windowRect, WinWindow, "Quit Window");
+        }
+        else if (windowSwitch == 2)
+        {
+            GUIAlphaColor_0_To_1();
+            windowRect = GUI.Window(0, windowRect, LoseWindow, "Quit Window");
+        }
+
+    }
+
+    void WinWindow(int windowID)
+    {
+        GUI.Label(new Rect(175, 50, 300, 30), "玩家2勝利!");
+
+        if (GUI.Button(new Rect(80, 110, 100, 20), "回到首頁"))
+        {
+            SceneManager.LoadScene("title");
+            Time.timeScale = 1f;
+        }
+        if (GUI.Button(new Rect(220, 110, 100, 20), "再玩一遍"))
+        {
+            SceneManager.LoadScene("game2");
+            Time.timeScale = 1f;
+        }
+        GUI.DragWindow();
+    }
+
+    //If lose is treiggered from TetroControl/void Update()/if (hp <= 0){}
+    public void EndLose()
+    {
+        windowSwitch = 2;
+        alpha = 0; // Init Window Alpha Color
+        Time.timeScale = 0f;
+    }
+
+    void LoseWindow(int windowID)
+    {
+        GUI.Label(new Rect(183, 50, 300, 30), "失敗!");
+
+        if (GUI.Button(new Rect(80, 110, 100, 20), "回到首頁"))
+        {
+            SceneManager.LoadScene("title");
+            Time.timeScale = 1f;
+        }
+        if (GUI.Button(new Rect(220, 110, 100, 20), "再玩一遍"))
+        {
+            SceneManager.LoadScene("game");
+            Time.timeScale = 1f;
+        }
+        GUI.DragWindow();
+    }
+
+}
---
+++ Assets/TwoPlayer/Script2/TetroControl3.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetroControl3 : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public Rigidbody2D rigid2D;
+
+    // NEW : 設定一個 flag，用來確認當前方塊是否 "第一次" 發生 OnCollisionEnter
+    private bool isFirstCollisionEnter = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // NEW : 初始化 設為 true
+        isFirstCollisionEnter = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFirstCollisionEnter == true)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                rigid2D.AddForce(new Vector2(-speed, 0), ForceMode2D.Force);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rigid2D.AddForce(new Vector2(speed, 0), ForceMode2D.Force);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                //rigid2D.AddForce(new Vector2(0, 1000f), ForceMode2D.Force);
+                transform.position += new Vector3(0, -1f, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.Rotate(0, 0, -90);
+            }
+        }
+    }
+
+    //To win
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Spawner3")
+        {
+            if (isFirstCollisionEnter == false)
+            {
+                FindObjectOfType<End3>().EndWin();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Create new tetros when colliding with Platform
+        if (collision.gameObject.tag == "Platform3")
+        {
+            if (isFirstCollisionEnter == true)
+            {
+                // NEW : 若為 第一次碰撞(isFirstCollisionEnter == true)，則進行 spawnNext() 產生新方塊
+                FindObjectOfType<TetroSpawn3>().spawnNext();
+
+                // NEW : 將 flag 設為 flase，確保不再觸發 spawnNext() 方法
+                isFirstCollisionEnter = false;
+            }
+        }
+        if (collision.gameObject.tag == "Tetro3")
+        {
+            if (isFirstCollisionEnter == true)
+            {
+                // NEW : 若為 第一次碰撞(isFirstCollisionEnter == true)，則進行 spawnNext() 產生新方塊
+                FindObjectOfType<TetroSpawn3>().spawnNext();
+
+                // NEW : 將 flag 設為 flase，確保不再觸發 spawnNext() 方法
+                isFirstCollisionEnter = false;
+            }
+        }
+        // Self-destroyed when colliding with Boxkiller
+        if (collision.gameObject.tag == "BoxKiller")
+        {
+            if (isFirstCollisionEnter == true)
+            {
+                Destroy(this.gameObject);
+
+                // NEW : 若為 第一次碰撞(isFirstCollisionEnter == true)，則進行 spawnNext() 產生新方塊
+                FindObjectOfType<TetroSpawn3>().spawnNext();
+
+                // NEW : 將 flag 設為 flase，確保不再觸發 spawnNext() 方法
+                isFirstCollisionEnter = false;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
+
