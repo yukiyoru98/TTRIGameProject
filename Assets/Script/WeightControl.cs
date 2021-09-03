5a9e3f9c5661e1cc0c72517c503e62c382a0061f
unknown <lf3.net@gmail.com>
2021-09-03 22:02:20 +0800

    weight control

---
+++ Assets/Script/WeightControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightControl : MonoBehaviour
+{
+    //private float StartPosition = GameObject.GetComponent<Transform>().position;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position -= new Vector3(0.02f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(0.02f, 0, 0);
+        }
+    }
+}
